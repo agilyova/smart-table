39f2ace48b51dd080fb8581a61e4d05a43b9ebf3
Aleksandra Gilyova <gilyova.a.v@gmail.com>
2025-11-03 18:55:20 +0300

    Added PW-6

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
+https://github.com/agilyova/smart-table
 # sp6-2_smart-table_starter
--- src/main.js
+++ src/main.js
@@ -7,22 +7,48 @@ import {initData} from "./data.js";
 import {processFormData} from "./lib/utils.js";
 
 import {initTable} from "./components/table.js";
+import {initPagination} from "./components/pagination.js";
+import {initSorting} from "./components/sorting.js";
+import {initFiltering} from "./components/filtering.js";
+import {initSearching} from "./components/searching.js";
+
 // @todo: подключение
 
 
 // Исходные данные используемые в render()
 const {data, ...indexes} = initData(sourceData);
 
+function getMinMaxTotalValues(data) {
+  let min = data[0].total;
+  let max = data[0].total;
+
+  data.forEach(el => {
+    min = Math.min(el.total, min);
+    max = Math.max(el.total, max);
+  });
+
+  return {min, max};
+}
+
 /**
  * Сбор и обработка полей из таблицы
  * @returns {Object}
  */
 function collectState() {
-    const state = processFormData(new FormData(sampleTable.container));
+  const state = processFormData(new FormData(sampleTable.container));
+  const rowsPerPage = parseInt(state.rowsPerPage);    // приведём количество страниц к числу
+  const page = parseInt(state.page ?? 1);// номер страницы по умолчанию 1 и тоже число
+  const minMaxTotal = getMinMaxTotalValues(data);
+  const totalFrom = parseFloat(state.totalFrom.replace(',', '.') || minMaxTotal.min);
+  const totalTo = parseFloat(state.totalTo.replace(',', '.') || minMaxTotal.max);
+  const total = [totalFrom, totalTo];
 
-    return {
-        ...state
-    };
+  return {                                            // расширьте существующий return вот так
+    ...state,
+    rowsPerPage,
+    page,
+    total
+  };
 }
 
 /**
@@ -30,23 +56,49 @@ function collectState() {
  * @param {HTMLButtonElement?} action
  */
 function render(action) {
-    let state = collectState(); // состояние полей из таблицы
-    let result = [...data]; // копируем для последующего изменения
-    // @todo: использование
+  let state = collectState(); // состояние полей из таблицы
+  let result = [...data]; // копируем для последующего изменения
+  // @todo: использование
 
+  result = applySearching(result, state, action);
+  result = applyFiltering(result, state, action);
+  result = applySorting(result, state, action);
+  result = applyPagination(result, state, action);
 
-    sampleTable.render(result)
+  sampleTable.render(result)
 }
 
 const sampleTable = initTable({
-    tableTemplate: 'table',
-    rowTemplate: 'row',
-    before: [],
-    after: []
+  tableTemplate: 'table',
+  rowTemplate: 'row',
+  before: ['search', 'header', 'filter'],
+  after: ['pagination']
 }, render);
 
 // @todo: инициализация
+const applyPagination = initPagination(
+  sampleTable.pagination.elements,             // передаём сюда элементы пагинации, найденные в шаблоне
+  (el, page, isCurrent) => {                    // и колбэк, чтобы заполнять кнопки страниц данными
+    const input = el.querySelector('input');
+    const label = el.querySelector('span');
+    input.value = page;
+    input.checked = isCurrent;
+    label.textContent = page;
+    return el;
+  }
+);
 
+const applySorting = initSorting([        // Нам нужно передать сюда массив элементов, которые вызывают сортировку, чтобы изменять их визуальное представление
+  sampleTable.header.elements.sortByDate,
+  sampleTable.header.elements.sortByTotal
+]);
+
+const applyFiltering = initFiltering(
+  sampleTable.filter.elements, {    // передаём элементы фильтра
+    searchBySeller: indexes.sellers                                    // для элемента с именем searchBySeller устанавливаем массив продавцов
+  });
+
+const applySearching = initSearching('search');
 
 const appRoot = document.querySelector('#app');
 appRoot.appendChild(sampleTable.container);
